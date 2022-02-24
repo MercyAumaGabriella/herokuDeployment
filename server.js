5c145fb8c3622b5eb09dea54645458ebb1049e69
Mercy Auma <aumagabriella@gmail.com>
2022-02-24 23:03:39 +0300

    Fourth commit: Server.js was updated

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express()
+const app = express();
 require("dotenv").config();
+const router = express.Router();
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> {
-    res.sendFile('index.html');
+router.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname+'index.html'));
 })
 
+app.use('/', router);
 app.listen(port, ()=>{
     console.log(`Example website running on port: ${port}`);
 })
